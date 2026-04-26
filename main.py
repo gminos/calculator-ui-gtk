b35dadf3f557e86d1abd8c1e2398d17c05e6f4fe
gminos <gjosebelarmino@gmail.com>
2026-04-26 10:57:42 -0500

    feat: implementar lógica de operadores y gestión de estados

--- main.py
+++ main.py
@@ -9,11 +9,21 @@ from gi.repository import Gtk, Gio
 class CalculatorWindow(Gtk.Window):
     __gtype_name__ = "CalculatorWindow"
 
+    def __init__(self) -> None:
+        super().__init__()
+        self.first_number: float
+        self.operator: str
+        self.waiting_for_second_number: bool = False
+
     display: Gtk.Entry = Gtk.Template.Child()
 
     @Gtk.Template.Callback()
     def on_digit_pressed(self, button: Gtk.Button) -> None:
         label = button.get_label()
+
+        if self.waiting_for_second_number:
+            self.display.set_text("")
+            self.waiting_for_second_number = False
 
         current_text = self.display.get_text()
         end_pos = self.display.get_text_length()
@@ -22,6 +32,12 @@ class CalculatorWindow(Gtk.Window):
             self.display.delete_text(0, end_pos)
 
         self.display.insert_text(label, self.display.get_text_length())
+
+    @Gtk.Template.Callback()
+    def on_operator_pressed(self, operator: Gtk.Button) -> None:
+        self.first_number = float(self.display.get_text() or "0")
+        self.operator = operator.get_label() or ""
+        self.waiting_for_second_number = True
 
     @Gtk.Template.Callback()
     def on_clear_pressed(self, _) -> None:
